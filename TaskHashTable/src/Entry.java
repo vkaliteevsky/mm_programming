04b2388da1820276deb73c36cb993752996e4582
Vasiliy <vkalit@gmail.com>
2012-12-08 18:40:13 +0400

    HashTable
    пусто

---
+++ TaskHashTable/src/Entry.java
@@ -0,0 +1,9 @@
+
+public class Entry {
+	private String key;
+	private int counter;
+
+	public Entry(String _key, int _counter){
+		
+	}
+}
---
+++ TaskHashTable/src/HashTable.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable {
+	private List<List<Entry>> table = new ArrayList<>();
+	private int TableSize;
+	
+	public HashTable(){
+		
+	}
+	
+	private HashFunction(){
+		
+	}
+	
+	private add(){
+		
+	}
+	
+	private find(){
+		
+	}
+
+	private delete(){
+		
+	}
+}
